0578d7cde85074ba4a52f39f987f3356f4d3988c
Jan Molnar <jan.molnar@math.uzh.ch>
2015-07-13 17:00:39 +0200

    Added much better asynchronous change handling.

--- Sample Apps/Robotroller/Robotics.Mobile.Robotroller.Core/Pages/DeviceDetail.xaml.cs
+++ Sample Apps/Robotroller/Robotics.Mobile.Robotroller.Core/Pages/DeviceDetail.xaml.cs
@@ -9,9 +9,9 @@ using Robotics.Messaging;
 using Robotics.Mobile.Core.Bluetooth.LE;
 
 namespace Robotics.Mobile.Robotroller
-{	
+{
 	public partial class DeviceDetail : TabbedPage
-	{	
+	{
 		readonly IAdapter adapter;
 		readonly Guid deviceId;
 
@@ -31,8 +31,59 @@ namespace Robotics.Mobile.Robotroller
 
 			adapter.DeviceDisconnected += (sender, e) => {
 				// if device disconnects, return to main list screen
-				Navigation.PopToRootAsync();
+				Navigation.PopToRootAsync ();
 			};
+
+			Task.Factory.StartNew (CommunicationWorker);
+		}
+
+		private double SpeedValue;
+		private double TurnValue;
+
+		private async Task CommunicationWorker ()
+		{
+			double lastSpeed = 0;
+			double lastTurn = 0;
+
+			while (true) {
+
+				try {
+
+					if (client != null) {
+						
+						var speed = SpeedValue;
+						var turn = TurnValue;
+
+						if (speed != lastSpeed) {
+							Debug.WriteLine("Worker::Updating Speed Value.");
+
+							var speedVariable = client.Variables.FirstOrDefault (x => x.Name == "Speed");
+							if (speedVariable != null) {
+								speedVariable.Value = speed;
+							}
+
+							lastSpeed = speed;
+						}
+
+						if (turn != lastTurn) {
+							Debug.WriteLine("Worker::Updating Turn Value.");
+
+							var turnVariable = client.Variables.FirstOrDefault (x => x.Name == "Turn");
+							if (turnVariable != null) {
+								turnVariable.Value = turn;
+							}
+
+							lastTurn = turn;
+						}
+					}
+					
+				} catch (Exception ex) {
+					Debug.WriteLine ("Worker:: " + ex.ToString ());
+				}
+
+				await Task.Delay (GyroUpdateInterval);
+			}
+					
 		}
 
 		public void OnVariableSelected (object sender, SelectedItemChangedEventArgs e)
@@ -60,7 +111,7 @@ namespace Robotics.Mobile.Robotroller
 		}
 
 		ControlClient client;
-			
+
 		async Task RunControlAsync ()
 		{
 			var cts = new CancellationTokenSource ();
@@ -82,11 +133,12 @@ namespace Robotics.Mobile.Robotroller
 
 
 		#region Joystick
+
 		// use device gyroscope for speed and direction
-		// gets wired/unwired when 
+		// gets wired/unwired when
 		IGyro boundGyro;
 		DateTime lastGyroUpdateTime = DateTime.Now;
-		static readonly TimeSpan GyroUpdateInterval = TimeSpan.FromSeconds (1.0/2);
+		static readonly TimeSpan GyroUpdateInterval = TimeSpan.FromSeconds (1.0 / 2);
 
 		void UnbindGyro ()
 		{
@@ -99,44 +151,58 @@ namespace Robotics.Mobile.Robotroller
 		void HandleGyroUpdated (object sender, EventArgs e)
 		{
 			var g = (IGyro)sender;
-			var now = DateTime.Now;
-			if (now - lastGyroUpdateTime > GyroUpdateInterval) {
-				lastGyroUpdateTime = now;
-				Task.Factory.StartNew (
-					() => {
-						if (client == null)
-							return;
 
-						var speed = Math.Min(1, Math.Max(Math.Cos (g.Pitch),0));
-						Debug.WriteLine ("Gyro.Pitch = " + g.Pitch + ", Speed = " + speed);
+									var speed = Math.Min (1, Math.Max (Math.Cos (g.Pitch), 0));
+									Debug.WriteLine ("Gyro.Pitch = " + g.Pitch + ", Speed = " + speed);
+			
+									var turn = Math.Sin (Math.Max (-Math.PI / 2, Math.Min (Math.PI / 2, g.Roll)));
+			//						Debug.WriteLine ("Gyro.Roll = " + g.Roll + ", Turn = " + turn);
+			
+									if (ReverseSwitch.IsToggled) {
+										speed = -speed;
+									}
 
-						var turn = Math.Sin (Math.Max (-Math.PI / 2, Math.Min (Math.PI / 2, g.Roll)));
-//						Debug.WriteLine ("Gyro.Roll = " + g.Roll + ", Turn = " + turn);
+			//SpeedValue = speed;
+			TurnValue = turn;
 
-						if (ReverseSwitch.IsToggled) {
-							speed = -speed;
-						}
-
-						var speedVariable = client.Variables.FirstOrDefault (x => x.Name == "Speed");
-						if (speedVariable != null) {
-							speedVariable.Value = (1+speed)/2;
-						}
-
-						var turnVariable = client.Variables.FirstOrDefault (x => x.Name == "Turn");
-						if (turnVariable != null) {
-							turnVariable.Value = turn;
-						}
-
-						// let's show the values we're sending to the robot
-						Device.BeginInvokeOnMainThread(() => {
-							JoystickOutputSpeed.Text = String.Format("Speed: {0}", Math.Round(speed,2));
-							JoystickOutputTurn.Text = String.Format("Turn: {0}", Math.Round(turn,2));
-						});
-					},
-					CancellationToken.None,
-					TaskCreationOptions.None,
-					scheduler);
-			}
+//			var now = DateTime.Now;
+//			if (now - lastGyroUpdateTime > GyroUpdateInterval) {
+//				lastGyroUpdateTime = now;
+//				Task.Factory.StartNew (
+//					() => {
+//						if (client == null)
+//							return;
+//
+//						var speed = Math.Min (1, Math.Max (Math.Cos (g.Pitch), 0));
+//						Debug.WriteLine ("Gyro.Pitch = " + g.Pitch + ", Speed = " + speed);
+//
+//						var turn = Math.Sin (Math.Max (-Math.PI / 2, Math.Min (Math.PI / 2, g.Roll)));
+////						Debug.WriteLine ("Gyro.Roll = " + g.Roll + ", Turn = " + turn);
+//
+//						if (ReverseSwitch.IsToggled) {
+//							speed = -speed;
+//						}
+//
+//						var speedVariable = client.Variables.FirstOrDefault (x => x.Name == "Speed");
+//						if (speedVariable != null) {
+//							speedVariable.Value = (1 + speed) / 2;
+//						}
+//
+//						var turnVariable = client.Variables.FirstOrDefault (x => x.Name == "Turn");
+//						if (turnVariable != null) {
+//							turnVariable.Value = turn;
+//						}
+//
+//						// let's show the values we're sending to the robot
+//						Device.BeginInvokeOnMainThread (() => {
+//							JoystickOutputSpeed.Text = String.Format ("Speed: {0}", Math.Round (speed, 2));
+//							JoystickOutputTurn.Text = String.Format ("Turn: {0}", Math.Round (turn, 2));
+//						});
+//					},
+//					CancellationToken.None,
+//					TaskCreationOptions.None,
+//					scheduler);
+//			}
 		}
 
 		void BindGyro ()
@@ -157,48 +223,32 @@ namespace Robotics.Mobile.Robotroller
 		{
 			UnbindGyro ();
 		}
+
+		public void OnSlidersJanChanged (object sender, EventArgs e)
+		{
+			SpeedValue = SpeedJanSlider.Value;
+		}
+
 		#endregion
 
 
 
 		#region Sliders
-		// hacky attempt to use sliders for speed and direction
+
 		public void OnSlidersChanged (object sender, EventArgs e)
 		{
-			// re-use the gyro update interval, but for slider value changes
-			var now = DateTime.Now;
-			if (now - lastGyroUpdateTime > GyroUpdateInterval) {
-				lastGyroUpdateTime = now;
-				Task.Factory.StartNew (
-					() => {
-						if (client == null)
-							return;
+			SpeedValue = SpeedSlider.Value;
 
-						var speed = SpeedSlider.Value;
-
-						var speedVariable = client.Variables.FirstOrDefault (x => x.Name == "Speed");
-						if (speedVariable != null) {
-							speedVariable.Value = speed;
-						}
-
-						var turn = TurnSlider.Value - 1;
-
-						var turnVariable = client.Variables.FirstOrDefault (x => x.Name == "Turn");
-						if (turnVariable != null) {
-							turnVariable.Value = turn;
-						}
-					},
-					CancellationToken.None,
-					TaskCreationOptions.None,
-					scheduler);
-			}
+			TurnValue = TurnSlider.Value - 1;
 		}
 
-		public void OnCenterClicked (object sender, EventArgs e) {
+		public void OnCenterClicked (object sender, EventArgs e)
+		{
 			TurnSlider.Value = 1;
 		}
 
-		public void OnResetClicked (object sender, EventArgs e) {
+		public void OnResetClicked (object sender, EventArgs e)
+		{
 			SpeedSlider.Value = 0;
 			TurnSlider.Value = 1;
 		}
